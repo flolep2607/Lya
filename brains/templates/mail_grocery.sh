2655116ecca6bfc4abed31c6268c8ef4a223250b
floflo2607 <flo.lep2607@gmail.com>
2018-04-03 21:00:28 +0200

    j

--- brains/templates/mail_grocery.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/pi/kalliope_config/script/sendmail.py -s="[kalliope] Liste de courses" -b="`grep +course ~/todo.txt`"
--- brains/templates/mail_orders.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-orders=$(grep "order:" ../brains/*.yml | awk -F ': ' '{print $3;}' | sed -e s/\"//g | sort)
-/home/pi/kalliope_config/script/sendmail.py -s="[kalliope] Ordres disponibles" -b="$orders"
